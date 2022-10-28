02fbfabed22dfe42bf8b43df0b3a52a6c4786642
YuanhaoDong-Jeffrey <Jeffrey.dyh@gmail.com>
2022-10-27 21:17:53 -0500

    second commit to Builder pattern

--- src/BuilderPattern/User.java
+++ src/BuilderPattern/User.java
@@ -15,11 +15,75 @@ public class User {
         this.address = builder.address;
     }
 
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getBirthday() {
+        return this.birthday;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
     public static class UserBuilder{
         private String firstName; // required
         private String lastName; // required
         private String birthday; // required
         private String phone = ""; // default value is a empty String
         private String address; // default value is null
+
+        public UserBuilder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder birthday(String birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public UserBuilder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public UserBuilder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public User build() {
+            if (this.firstName == null || this.lastName == null) {
+                throw new IllegalArgumentException("Required fields are not set");
+            }
+
+            return new User(this);
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        User.UserBuilder builder =  new User.UserBuilder();
+        User cur = builder
+                .firstName("Yuanhao")
+                .lastName("Dong").build();
+
+        System.out.println(cur.getFirstName());
     }
 }
